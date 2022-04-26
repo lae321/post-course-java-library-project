25fda9c2abd3b8e31b3461df6472ce1a53d5318b
lae321 <lae321@hotmail.co.uk>
2022-04-26 16:03:54 +0100

    Begun writing CLI functions.

--- src/main/java/com/nology/Main.java
+++ src/main/java/com/nology/Main.java
@@ -8,12 +8,42 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
+  static Scanner scanner = new Scanner(System.in);
+
+  public static void login() {
+    System.out.println(
+        "Welcome to the Library System. Press 1 to login using an existing "
+            + "account. Press 2 to create a new account."
+            + " ");
+    String loginOrSignup = scanner.nextLine();
+    switch (loginOrSignup) {
+      case "1":
+        System.out.println("Hello, existing user.");
+        System.out.println("Please enter your username below.");
+        System.out.println("Username:");
+        String existingUser = scanner.nextLine();
+        // checkUser(existingUser);
+        break;
+      case "2":
+        System.out.println("Hello, new user.");
+        System.out.println("Please enter your desired username below.");
+        System.out.println("Username:");
+        String newUser = scanner.nextLine();
+        // createUser(newUser);
+        System.out.println("Success! A new account for " + newUser + " has been created.");
+        System.out.println("Press any key to return to the login page.");
+        scanner.nextLine();
+        login();
+        break;
+      default:
+        System.out.println("Invalid input. Press any key to return to the welcome page.");
+        scanner.nextLine();
+        login();
+    }
+  }
 
   public static void main(String[] args) throws IOException {
     String csvFile = "C:\\Users\\lae32\\Desktop\\sandbox\\java-library-project\\books_data.csv";
@@ -35,23 +65,22 @@ public class Main {
       mapper.enable(SerializationFeature.INDENT_OUTPUT);
       mapper.writeValue(new File("src/books_data.json"), list);
 
-      List<Book> bookList = mapper.readValue(new File("C:\\Users\\lae32\\Desktop\\sandbox\\java-library-project\\src" +
-          "\\books_data.json"), new TypeReference<>() {});
-
-      for (Book book : bookList ) {
-        System.out.println(book);
+      List<Book> bookList =
+          mapper.readValue(
+              new File(
+                  "C:\\Users\\lae32\\Desktop\\sandbox\\java-library-project\\src"
+                      + "\\books_data.json"),
+              new TypeReference<>() {});
     }
-
-
-    }
-
+    /*
+    TODO:
+      Login functions:
+        existingUser() - takes entered username and presents options including view loans and
+        loan book. If username entered is admin, the user is prompted for the admin password.
+        Admin options are presented following authentication including report.
+        newUser() - takes entered username and checks whether it already exists in the users List
+        . If not, the user is created.
+    */
+    login();
   }
-  /*
-  TODO:
-    Parse JSON into individual Java objects
-    Store these objects in an ArrayList
-    Create login screen using scanner - admin, existing user, new user
-    Allow admins to create reports
-    Allow users to loan and return books
-  */
 }
